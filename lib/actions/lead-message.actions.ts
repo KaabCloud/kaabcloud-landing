2bb7b43d87ebe8af480725775709503a6d6176f7
JAlejandroRP <jesus.98_rdz@hotmail.com>
2025-05-31 16:57:30 -0600

    adding create lead message action

---
+++ lib/actions/lead-message.actions.ts
@@ -0,0 +1,19 @@
+'use server'
+
+import prisma from "../database/prisma";
+import { Prisma } from "../generated/prisma";
+
+export async function createLeadMessage(leadMessageData: Prisma.LeadMessageCreateInput) {
+  try {
+    const leadMessage = prisma.leadMessage.create({ data: leadMessageData })
+
+    return {
+      data: leadMessage
+    }
+  } catch (error) {
+    console.error(error)
+    return {
+      error: `Error while creating lead message ${error}`
+    }
+  }
+}
