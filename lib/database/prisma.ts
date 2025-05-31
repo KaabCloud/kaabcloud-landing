e051d0b62a50ac57aa1685604681a6545d3be6d7
JAlejandroRP <jesus.98_rdz@hotmail.com>
2025-05-31 16:57:21 -0600

    adding prisma singleton

---
+++ lib/database/prisma.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "../generated/prisma"
+
+// Crear el cliente de Prisma
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+// Declaración global para evitar crear múltiples instancias en desarrollo
+declare const globalThis: {
+  prismaGlobal: PrismaClient
+} & typeof global
+
+// Verificar si ya existe una instancia global en desarrollo (para evitar múltiples instancias)
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+// Asignar la instancia global solo en desarrollo
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prismaGlobal = prisma
+}
+
+export default prisma;
