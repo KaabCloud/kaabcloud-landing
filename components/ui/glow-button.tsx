ad772bf4e6b65916ee00ecefd95927702198261c
JAlejandroRP <jesus.98_rdz@hotmail.com>
2025-05-31 21:52:20 -0600

    fix glow button margin

--- components/ui/glow-button.tsx
+++ components/ui/glow-button.tsx
@@ -10,7 +10,7 @@ interface GlowButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 const GlowButton = forwardRef<HTMLButtonElement, GlowButtonProps>(
   ({ className, variant = "default", size = "default", ...props }, ref) => {
     return (
-      <div className="relative group">
+      <div className="relative group m-1">
         <div className="absolute -inset-0.5 rounded-lg overflow-hidden">
           <div className="absolute inset-0 bg-gradient-to-r from-transparent via-pink-500 to-transparent animate-shimmer"></div>
         </div>
