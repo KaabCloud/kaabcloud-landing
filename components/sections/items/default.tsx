bbdaa7228fcb12018185af13734a5b52f6b59294
JAlejandroRP <jesus.98_rdz@hotmail.com>
2025-06-01 19:40:14 -0600

    increasing max width

--- components/sections/items/default.tsx
+++ components/sections/items/default.tsx
@@ -52,7 +52,7 @@ export default function Items({
 }: ItemsProps) {
   return (
     <Section id={id} className={className}>
-      <div className="max-w-container mx-auto flex flex-col items-center gap-6 sm:gap-20">
+      <div className="max-w-4xl lg:max-w-6xl mx-auto flex flex-col items-center gap-6 sm:gap-20">
         <h2 className="max-w-[560px] text-center text-3xl leading-tight font-semibold sm:text-5xl sm:leading-tight">
           {title}
         </h2>
