78cdcccd66e6899b7250b170b12e5d3e5642f594
JAlejandroRP <jesus.98_rdz@hotmail.com>
2025-06-01 18:07:59 -0600

    fix link

--- components/sections/hero/default.tsx
+++ components/sections/hero/default.tsx
@@ -77,7 +77,7 @@ export default function Hero({
       icon: <Whatsapp className="mr-2 size-4" />
     },
     {
-      href: siteConfig.links.github,
+      href: '#contact',
       text: "Contactanos",
       variant: "glow",
       icon: <Mail className="mr-2 size-4" />
